6239742477eef6f4bc6aa357ee44b54162cf2477
psundar001yahoo <48411756+psundar001yahoo@users.noreply.github.com>
2019-03-11 01:35:09 +0530

    res (#1)
    uu

--- bb/src/App.js
+++ bb/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Maked public
+            Maked public new branch
           </a>
         </header>
       </div>
